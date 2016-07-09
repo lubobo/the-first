428ea6434ab81e0e4966928a50bbc10140ec3355
lukbob <m18240438909_1@163.com>
2016-07-09 22:31:12 +0800

    修改数据库登陆密码

--- writing/login.php
+++ writing/login.php
@@ -1,5 +1,5 @@
 <?php
-  $conn= new mysqli('localhost','root','13653917lgbdqq');
+  $conn= new mysqli('localhost','root','passwords');
   if ($conn->connect_error) {
   	die("数据库连接失败".$conn->connect_error);
   }
